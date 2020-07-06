b1e16b8584c17961f8a399fc6e6053f1b68cb936
Gesiane <gesianef@hotmail.com>
2020-07-06 13:28:57 +0300

    adding files

---
+++ confusion/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component{
+    render(){
+        return(
+            <div className="App">
+                <header className="App-header">
+                    <img scr={logo} className="App-logo" alt="logo" /> 
+                    <h1 className="App.title"> Welcome to React</h1>
+                </header>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.ja</code> and sa...
+                </p>
+            </div>
+        );
+    }
+
+}
+
+export default App;
