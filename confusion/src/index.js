070103adbb492de1c9daa16ae8cbe0aeb815204f
Gesiane <gesianef@hotmail.com>
2020-07-06 17:34:32 +0300

    component#1

--- confusion/src/index.js
+++ confusion/src/index.js
@@ -5,9 +5,5 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-/* importing Bootstrap first and then the index.css later. 
-So if I add my own customization CSS classes, that can 
-override the default Bootstrap classes if required.*/
-
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
